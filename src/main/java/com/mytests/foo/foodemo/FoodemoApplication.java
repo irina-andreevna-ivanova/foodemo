62c0a1befd653708fe2ee19b0f733a34f4f8eed0
irina <irina@me.com>
2018-08-28 17:20:51 +0300

    Initial commit

---
+++ src/main/java/com/mytests/foo/foodemo/FoodemoApplication.java
@@ -0,0 +1,12 @@
+package com.mytests.foo.foodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodemoApplication.class, args);
+    }
+}
